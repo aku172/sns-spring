4ee0dd9c158ca5626f0a57098e3b4a060301cb05
aku172 <ashwinikumar2545@gmail.com>
2020-11-17 02:13:38 +0530

    Adding subscription programmatically

--- sns-subscriber-simple/src/main/java/com/ashwini/sns/subscriber/config/SnsConfig.java
+++ sns-subscriber-simple/src/main/java/com/ashwini/sns/subscriber/config/SnsConfig.java
@@ -1,5 +1,10 @@
 package com.ashwini.sns.subscriber.config;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +14,8 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.sns.AmazonSNSAsync;
 import com.amazonaws.services.sns.AmazonSNSAsyncClientBuilder;
+import com.amazonaws.services.sns.model.SubscribeRequest;
+import com.amazonaws.services.sns.model.SubscribeResult;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -50,5 +57,24 @@ public class SnsConfig {
 	public NotificationMessagingTemplate notificationMessagingTemplate() {
 		return new NotificationMessagingTemplate(asyncSnsClient());
 	}
-
+	
+	//This is for registering subscription programmatically, not by console
+	//The only issue is endpoint.
+	@PostConstruct
+	public void subscribeTopic() {
+		SubscribeRequest subscriptionRequest = new SubscribeRequest();
+		subscriptionRequest.setProtocol("http");
+		subscriptionRequest.setEndpoint("http://bc5c816052fe.ngrok.io/subscribe");
+		subscriptionRequest.setTopicArn("arn:aws:sns:us-east-1:101828129045:TestTopic");
+		
+		Future<SubscribeResult> future = asyncSnsClient().subscribeAsync(subscriptionRequest);
+		
+		try {
+			SubscribeResult subscribeResult = future.get();
+			System.out.println();
+		} catch (InterruptedException | ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
